9ed7f67c8c4480b3835679c837395218b93dffc4
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-03-18 22:48:55 +0100

    ud

--- src/services/mail/mail.service.js
+++ src/services/mail/mail.service.js
@@ -3,22 +3,26 @@ const nodemailer = require('nodemailer')
 const config = require('../../config')
 
 const send = async (from, to, subject, text, html, attachments) => {
+  const message = {
+    from,
+    to,
+    subject,
+    text,
+    html,
+    attachments
+  }
   try {
     const transporter = nodemailer.createTransport(config.nodemailer)
-    const result = await transporter.sendMail({
-      from,
-      to,
-      subject,
-      text,
-      html,
-      attachments
-    })
+    const result = await transporter.sendMail(message)
     if (config.debug === 'true') {
       console.log(result)
     }
     return result
   } catch (e) {
     console.error(e.message)
+    if (config.debug === 'true') {
+      console.log(message)
+    }
     return {
       error: e.message
     }
