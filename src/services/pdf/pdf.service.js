8d10832a005c18e6ccfd1541a07fa4ad84b82f04
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-24 22:02:25 +0200

    up

--- src/services/pdf/pdf.service.js
+++ src/services/pdf/pdf.service.js
@@ -17,7 +17,7 @@ const generateQrCodeHtml = async url => {
 const generateVerificationHtml = async url => {
   const qrCodeHtml = await generateQrCodeHtml(url)
   const link = `<a href="${url}" style="color: #000000;">${url}</a>`
-  const verificationHtml = `<div style="width: 100%; display: flex; justify-content: center; align-items: center; ">${qrCodeHtml}<div><div style="font-size: 10px">To verify this certificate, scan the QR code or open this link:<br />${link}</div><div style="margin-top: 20px; font-size: 10px; text-align: right;">Powered by <a href="https://auther.org" style="color: #000000;">Auther</a></div></div></div>`
+  const verificationHtml = `<div style="width: 100%; display: flex; justify-content: center; align-items: center; ">${qrCodeHtml}<div><div style="font-size: 8px">To verify this certificate, scan the QR code or open this link: ${link}</div><div style="margin-top: 20px; font-size: 8px; text-align: right;">Powered by <a href="https://auther.org" style="color: #000000;">Auther</a></div></div></div>`
   return verificationHtml
 }
 
