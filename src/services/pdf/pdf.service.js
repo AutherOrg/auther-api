90911ae3f6e4b76be778f522caf008dfcfe24b19
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-26 16:04:05 +0200

    up

--- src/services/pdf/pdf.service.js
+++ src/services/pdf/pdf.service.js
@@ -6,8 +6,8 @@ const engine = require('./phantomjs.engine.pdf.service')
 
 const generateQrCodeHtml = async url => {
   try {
-    const qrCodeDataUrl = await qrCode.toDataURL(url, { width: 100 })
-    const qrCodeHtml = `<img src="${qrCodeDataUrl}" alt="QR code" />`
+    const qrCodeDataUrl = await qrCode.toDataURL(url)
+    const qrCodeHtml = `<img src="${qrCodeDataUrl}" alt="QR code" style="height: 50px; width: auto;" />`
     return qrCodeHtml
   } catch (e) {
     console.error(e)
@@ -19,7 +19,7 @@ const generateVerificationHtml = async url => {
   const verificationLink = `<a href="${url}" style="${config.pdf.style.verification.link}">${url}</a>`
   const verificationTextAndLink = `<div style="${config.pdf.style.verification.text}">To verify this certificate, scan the QR code or open this link: ${verificationLink}</div>`
   const autherLink = `<a href="https://auther.org" style="${config.pdf.style.verification.link}">Auther</a>`
-  const autherTextAndLink = `<div style="${config.pdf.style.verification.text} margin-top: 16px; text-align: right;">Powered by ${autherLink}, an opensource implementation of Blockcerts</div>`
+  const autherTextAndLink = `<div style="${config.pdf.style.verification.text} margin-top: 8px; text-align: right;">Powered by ${autherLink}, an opensource implementation of Blockcerts</div>`
   const verificationAndAuther = `<div>${verificationTextAndLink}${autherTextAndLink}</div>`
   const verificationHtml = `<div style="${config.pdf.style.verification.wrapper}">${qrCodeHtml}${verificationAndAuther}<div>`
   return verificationHtml
