10926b83f61fd05f2263d8144be2049de2997a1f
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-26 12:24:54 +0200

    test

--- src/services/pdf/phantomjs.engine.pdf.service.js
+++ src/services/pdf/phantomjs.engine.pdf.service.js
@@ -4,7 +4,7 @@ const config = require('../../config')
 
 const html2pdf = (certificateHtml, verificationHtml) => {
   return new Promise((resolve, reject) => {
-    const html = `<html style="height: 0; transform-origin: 0 0; -webkit-transform-origin: 0 0; transform: scale(0.53); -webkit-transform: scale(0.53)";><body style="font-size: 50%;"><div style="${config.pdf.style.wrapper}">${certificateHtml}${verificationHtml}</div></body></html>`
+    const html = `<div style="${config.pdf.style.wrapper}">${certificateHtml}${verificationHtml}</div>`
     htmlPdf.create(html, {
       format: 'A4',
       orientation: 'landscape',
